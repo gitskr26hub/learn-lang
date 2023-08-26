751f865707afc641e6b693f5f1284fc29b784382
Sugam Kumar <103991801+gitskr26hub@users.noreply.github.com>
2023-08-26 18:45:32 +0530

    Update App.tsx

--- learn-lang/src/App.tsx
+++ learn-lang/src/App.tsx
@@ -1,14 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Learning from "./components/Learning";
 import Quiz from "./components/Quiz";
 import Result from "./components/Result";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import Loader from "./components/Loader";
 import "./Style.css"
+import { useSelector } from "react-redux";
 
 const App = () => {
+
+
+  const navigate=useNavigate()
+  const {words}=useSelector((state:{root:StateType})=>state.root)
+
+  useEffect(()=>{
+    navigate("/")
+  },[words.length==0])
+
+
   return (
     <div>
       <Header />
