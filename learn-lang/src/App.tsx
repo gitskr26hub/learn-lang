7afb35ce56d0198c756c651a548101313ca49395
Sugam Kumar <103991801+gitskr26hub@users.noreply.github.com>
2023-08-31 10:56:02 +0530

    Update App.tsx

--- learn-lang/src/App.tsx
+++ learn-lang/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes,Switch  } from "react-router-dom";
+import { Route, Routes  } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Learning from "./components/Learning";
@@ -18,7 +18,7 @@ const App = () => {
 
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route  path="/" element={<Home />} />
           <Route path="/learn" element={<Learning />} />
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/result" element={<Result />} />
